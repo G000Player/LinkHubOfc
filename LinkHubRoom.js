fbddaf0498fbd7c3e52e9a620de4da8b5c5e555d
mgMarcos <141787891+G000Player@users.noreply.github.com>
2024-01-11 21:23:30 -0300

    Add files via upload

---
+++ LinkHubRoom.js
@@ -0,0 +1,36 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDPtSOaMO779w601-4o5QyGO6n4JtCSADQ",
+    authDomain: "linkhub-17bda.firebaseapp.com",
+    projectId: "linkhub-17bda",
+    storageBucket: "linkhub-17bda.appspot.com",
+    messagingSenderId: "302961447866",
+    appId: "1:302961447866:web:30a9688ae9867e49a80383"
+  };  
+    firebase.initializeApp(firebaseConfig);
+
+        userName = localStorage.getItem("userName");
+
+    document .getElementById ("userName") .innerHTML = "Bem-vindo(a) " + userName + "!";
+
+    function addRoom()
+    {
+     roomName = document.getElementById("roomName").value;
+
+     firebase.database().ref("/").child(roomName).update({
+        purpose : "adicionar nome de sala"
+     });
+
+        localStorage.setItem("roomName", roomName);
+
+        window.location = "kwitterPage.html";
+    }
+
+    function getData() { firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+        roomNames = childKey;
+        console.log("Nome da Sala - " + roomNames);
+       row = "<div class='roomName' id="+roomNames+" onclick='redirectToRoomName(this.id)' >#"+ roomNames +"</div><hr>";
+       document. getElementById("output").innerHTML += row;
+     });
+    });
+
+}
